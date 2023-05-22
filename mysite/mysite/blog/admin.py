ccb47ef4466d1045d5dc6060c4ebe2b8f7e94c89
sanch13 <sanch13sanch@gmail.com>
2023-05-22 10:44:48 +0300

    changing of the admin

--- mysite/mysite/blog/admin.py
+++ mysite/mysite/blog/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]  # позволяет задавать поля
+    # модели, которые вы хотите показывать на странице django admin
+    list_filter = ["status", "created", "publish", "author"]  # позволяет фильтровать результаты
+    # по полям
+    search_fields = ["title", "body"]  # поиск по полям
+    prepopulated_fields = {"slug": ("title",)}  # автомат. заполнять поле slug данными поле title
+    raw_id_fields = ["author"]  # поле author отображается поисковым виджетом
+    date_hierarchy = "publish"  # для навигации по иерархии дат
+    ordering = ["status", "publish"]  # критерии сортировки по умолчанию
+
+
