50f191f050f4c00ed225dbc1bf1c4ffa0319a5d1
sanch13 <sanch13sanch@gmail.com>
2023-05-22 17:21:42 +0300

    first template + requirements

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()  # извлек. все посты со статусом PUBLISHED, исп. менеджер published
+    return render(request=request,
+                  template_name="blog/post/list.html",
+                  context={"posts": posts})
+
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.objects.get(pk=id)  # извлек. объект Post с заданным id
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found")
+    post = get_object_or_404(Post,
+                             pk=id,
+                             status=Post.Status.PUBLISHED)
+    #  get_object_or_404 - функцию сокращенного доступа для вызова метода
+    # get() в заданном модельном менеджере и вызова исключения Http404
+    return render(request=request,
+                  template_name="blog/post/detail.html",
+                  context={"post": post})
