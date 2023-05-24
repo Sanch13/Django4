78fb1b60020a7bf9d2ee5fd82c81bef173e3e8df
sanch13 <sanch13sanch@gmail.com>
2023-05-24 20:58:50 +0300

    add comments in the posts

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -3,44 +3,43 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.conf import settings
-
-from .froms import EmailPostForm
-from .models import Post
+from django.views.decorators.http import require_POST
+from .froms import EmailPostForm, CommentForm
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
-    posts = Post.published.all()
-    return render(request=request, template_name="blog/base.html", context={"posts": posts})
+    return render(request=request, template_name="blog/base.html")
 
 
-def post_list(request):
-    post_list = Post.published.all()  # извлек. все посты со статусом PUBLISHED, исп. менеджер
-    # published
-    # # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)  # paginator - экземпляр класса Paginator с числом объектов,
-    # возвращаемых в расчете на страницу
-    page_number = request.GET.get("page", 1)  # извлекаем HTTP GET-параметр page и сохраняем его в
-    # переменной page_number. Если параметра page нет в GET-параметрах запроса, используем
-    # стандартное значение 1, чтобы загрузить первую страницу результатов.
-    try:
-        posts = paginator.page(page_number)  # получ. объекты для желаем. стр., вызывая метод page()
-    except PageNotAnInteger:  # Если page_number не целое число, то выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:  # Если page_number находится вне диапазона, то выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)  # paginator.num_pages - общее число страниц
-    return render(request=request,
-                  template_name="blog/post/list.html",
-                  context={"posts": posts})
+# def post_list(request):
+#     post_list = Post.published.all()  # извлек. все посты со статусом PUBLISHED, исп. менеджер
+#     # published
+#     # # Постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list, 3)  # paginator - экземпляр класса Paginator с числом объектов,
+#     # возвращаемых в расчете на страницу
+#     page_number = request.GET.get("page", 1)  # извлекаем HTTP GET-параметр page и сохраняем его в
+#     # переменной page_number. Если параметра page нет в GET-параметрах запроса, используем
+#     # стандартное значение 1, чтобы загрузить первую страницу результатов.
+#     try:
+#         posts = paginator.page(page_number)  # получ. объекты для желаем. стр., вызывая метод page()
+#     except PageNotAnInteger:  # Если page_number не целое число, то выдать первую страницу
+#         posts = paginator.page(1)
+#     except EmptyPage:  # Если page_number находится вне диапазона, то выдать последнюю страницу
+#         posts = paginator.page(paginator.num_pages)  # paginator.num_pages - общее число страниц
+#     return render(request=request,
+#                   template_name="blog/post/list.html",
+#                   context={"posts": posts})
 
-# class PostListView(ListView):  # ListView позволяет перечислять объекты любого типа
-#     """    Альтернативное представление списка постов    """
-#     queryset = Post.published.all()  # атрибут queryset, нужен для того, чтобы иметь
-#     # конкретно-прикладной набор запросов QuerySet, не извлекая все объекты базы данных
-#     context_object_name = "posts"  # контекстная переменная posts исп. для результатов запроса
-#     # queryset. Если не указ. имя контекстного объекта то по умолчанию исп. переменная object_list
-#     paginate_by = 3  # задается постраничная разбивка результатов с возвратом 3 объектов на страницу
-#     template_name = "blog/post/list.html"  # путь + шаблон котор. исп. для прорисовки страницы
+class PostListView(ListView):  # ListView позволяет перечислять объекты любого типа
+    """    Альтернативное представление списка постов    """
+    queryset = Post.published.all()  # атрибут queryset, нужен для того, чтобы иметь
+    # конкретно-прикладной набор запросов QuerySet, не извлекая все объекты базы данных
+    context_object_name = "posts"  # контекстная переменная posts исп. для результатов запроса
+    # queryset. Если не указ. имя контекстного объекта то по умолчанию исп. переменная object_list
+    paginate_by = 3  # задается постраничная разбивка результатов с возвратом 3 объектов на страницу
+    template_name = "blog/post/list.html"  # путь + шаблон котор. исп. для прорисовки страницы
 
 
 def post_detail(request, year, month, day, post):
@@ -56,9 +55,13 @@ def post_detail(request, year, month, day, post):
                              slug=post)
     #  get_object_or_404 - функцию сокращенного доступа для вызова метода
     # get() в заданном модельном менеджере и вызова исключения Http404
+    comments = post.comments.filter(active=True)
+    # извлекаем все активные комментарии к посту. используем объект post, чтобы извлекать
+    # связанные объекты Comment.используя атрибут related_name поля ForeignKey в модели Post
+    form = CommentForm()
     return render(request=request,
                   template_name="blog/post/detail.html",
-                  context={"post": post})
+                  context={"post": post, "form": form, "comments": comments})
 
 
 def post_share(request, post_id):
@@ -91,3 +94,23 @@ def post_share(request, post_id):
     return render(request=request,
                   template_name="blog/post/share.html",
                   context={"post": post, "form": form, "sent": sent},)
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None  # перем. будет исп. для хранения комментарного объекта при его создании
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        # Метод save() создает экземпляр модели, к которой форма привязана, и сохраняет его в базе
+        # данных. Если вызывать его, используя commit=False, то экземпляр модели создается, но не
+        # сохраняется в базе данных. Такой подход позволяет видоизменять объект перед его
+        # окончательным сохранением.
+        comment.post = post
+        comment.save()
+    return render(request=request,
+                  template_name="blog/post/comment.html",
+                  context={"post": post, "form": form, "comment": comment})
