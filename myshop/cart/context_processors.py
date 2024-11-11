4d56458b0a7b1ed66ccd81f5546c69b4b5cc1f86
sanch13 <sanch13sanch@gmail.com>
2024-11-11 18:05:37 +0300

    new start

--- myshop/cart/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .cart import Cart
-
-
-def cart(request):
-    """создается экземпляр класса Сart с объектом request в качестве параметра и обеспечивается
-    его доступность для шаблонов в виде переменной cart"""
-    return {"cart": Cart(request=request)}
